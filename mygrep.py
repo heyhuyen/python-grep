49184bd57d9521dcad9f32d2711dde7a9a9b85be
Huyen Tran <hey.huyen@gmail.com>
2012-12-06 15:32:26 -0500

    Add myfind(pattern, string)
    - use myfind() instead of builtin str.find()
    - test: time python mygrep.py square moby_dick.txt
    - mygrep stats: real 0.933s, user 0.917s, cpu 0.013s

--- mygrep.py
+++ mygrep.py
@@ -1,5 +1,14 @@
 import sys
 
+def myfind(pattern, string):
+    for i in range(len(string)):
+        for j in range(len(pattern)):
+            if string[i+j] != pattern[j]:
+                break
+            elif j == len(pattern) - 1:
+                return True
+    return False
+
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         pattern, filename = sys.argv[1:]
@@ -11,6 +20,6 @@ if __name__ == '__main__':
     f = open(filename)
     lines = f.readlines()
     for line in lines:
-        if line.find(pattern) != -1:
+        if myfind(pattern, line):
             print line.strip()
 
