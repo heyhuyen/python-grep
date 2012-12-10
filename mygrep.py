5dbfd3b976b9aa5f683a51397c6c9ca7cba896b1
Huyen Tran <hey.huyen@gmail.com>
2012-12-10 13:01:43 -0500

    Accept standard input
    - make filename argument optional
    - read from sys.stdin if filename not passed in
    - works with piping echo and cat
    - change line reading: read one line per loop instead of reading all lines and looping over lines in list in order to mimic grep behavior

--- mygrep.py
+++ mygrep.py
@@ -26,15 +26,21 @@ def myfind(pattern, string, color):
     return result
 
 if __name__ == '__main__':
+    # set up argparse argument parser
     parser = argparse.ArgumentParser(description="Find occurrences of a pattern in a file.")
     parser.add_argument('pattern', metavar="PATTERN", type=str, help="the pattern to find")
-    parser.add_argument('filename', metavar="FILENAME", type=argparse.FileType('r'), help="the file to search")
-    parser.add_argument('--color', metavar="BOOL", type=bool, nargs="?", const=True, help="highlight pattern in output")
+    parser.add_argument('filename', metavar="FILENAME", type=argparse.FileType('r'), nargs="?", default=sys.stdin, help="the file to search")
+    parser.add_argument('--color', type=bool, nargs="?", const=True, help="highlight pattern in output")
+
+    # unpack args
     args = parser.parse_args()
     pattern = args.pattern
     f = args.filename
-    lines = f.readlines()
-    for line in lines:
+
+    # read lines
+    line = f.readline()
+    while line:
         result = myfind(pattern, line, args.color)
         if len(result) > 0:
             print result.strip()
+        line = f.readline()
