272b7b3ea97061d945613222c65d814f29c4e032
Huyen Tran <hey.huyen@gmail.com>
2012-12-11 16:05:24 -0500

    Minor refactor and decomp ignore case
    - ignore case has own function
    - find fn var passed around
    - remove plain_find function
    - consolidated simple if statements into conditional expressions
    - line number code still ugly

--- mygrep.py
+++ mygrep.py
@@ -5,49 +5,32 @@ import pdb
 GREEN = '\033[92m'
 END = '\033[0m'
 
-# plain vanilla version
-# returns True first match
-# False if not found
-def plain_find(pattern, string, ignore):
-    string_mod = string
-    if ignore:
-        pattern = pattern.lower()
-        string_mod = string.lower()
+def index_find(pattern, string):
+    """Find first occurence of pattern in string."""
     for i in range(len(string)):
         for j in range(len(pattern)):
-            if string_mod[i+j] != pattern[j]:
-                break
-            elif j == len(pattern) - 1:
-                return string
-    return ''
-
-# plain vanilla version returning index
-def index_find(pattern, string, ignore):
-    string_mod = string
-    if ignore:
-        pattern = pattern.lower()
-        string_mod = string.lower()
-    for i in range(len(string)):
-        for j in range(len(pattern)):
-            if string_mod[i+j] != pattern[j]:
+            if string[i+j] != pattern[j]:
                 break
             elif j == len(pattern) - 1:
                 return i
     return -1
 
-# adds color
-def color_find(pattern, string, ignore):
+def ignore_find(pattern, string):
+    """Ignore case."""
+    return index_find(pattern.lower(), string.lower())
+
+def color_find(pattern, string, find):
+    """Find and color all occurences of pattern in string."""
     result = ''
-    index = index_find(pattern, string, ignore)
-    if index == -1:
-        return result
+    index = find(pattern, string)
+
     while index != -1:
         result += string[:index]
         result += GREEN + string[index:index + len(pattern)] + END
         string = string[index + len(pattern):]
-        index = index_find(pattern, string, ignore)
-    result += string
-    return result
+        index = find(pattern, string)
+
+    return result if result == '' else result + string
 
 if __name__ == '__main__':
     # set up argparse argument parser
@@ -63,19 +46,30 @@ if __name__ == '__main__':
     pattern = args.pattern
     f = args.text
 
+    # assign find fn
+    find = index_find
+    if args.ignore_case:
+        find = ignore_find
+
     # read lines
     line = f.readline()
-    line_n = 0
+    line_n = 0 # counter for line number option
     while line:
+        # ugly line number chunk
         n_str = ''
         if args.line_number:
             line_n += 1
             n_str += '%d:' % line_n
+
         if args.color:
-            result = color_find(pattern, line, args.ignore_case)
+            result = color_find(pattern, line, find)
         else:
-            result = plain_find(pattern, line, args.ignore_case)
+            index = find(pattern, line)
+            result = line if index != -1 else ''
+
+        # print to stdout and read next line
         if len(result) > 0:
             print n_str + result.strip()
         line = f.readline()
+
     f.close()
