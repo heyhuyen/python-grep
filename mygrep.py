0fdcf8a8c4fbe1085c723a77603aff64c6bcf756
Huyen Tran <hey.huyen@gmail.com>
2012-12-12 18:11:19 -0500

    Accept directories in list of filenames to search.
    - replace argparse's FileType with custom callback 'pathname' in 'files' arg, thus ditching all the nice features that argparse's FileType does
    - print 'Is a directory' if a directory is encountered, following grep's behavior
    - runs noticeably slower

--- mygrep.py
+++ mygrep.py
@@ -1,5 +1,4 @@
-import sys
-import fileinput
+import sys, os
 from argparse import ArgumentParser, FileType
 import pdb
 
@@ -42,27 +41,55 @@ def print_result(result, no_header, header, lineno):
         sys.stdout.write('%d:' % lineno)
     sys.stdout.write(result)
 
+def search_lines(text, find, line_number, color, no_filename):
+    line = text.readline()
+    lineno = 0 # counter for line number option
+    # read lines of a text
+    while line:
+        if line_number: # ugly lineno chunk
+            lineno += 1
+
+        if color:
+            result = color_find(pattern, line, find)
+        else:
+            index = find(pattern, line)
+            result = line if index != -1 else ''
+
+        # print to stdout and read next line
+        if len(result) > 0:
+            print_result(result, no_filename, text.name, lineno)
+        line = text.readline()
+
+    text.close()
+
+def pathname(path):
+    if os.path.isfile(path):
+        return path
+    else:
+        return 'mygrep: %s: Is a directory\n' % path
+
 def setup_parser():
     parser = ArgumentParser(description='Find occurrences of a pattern in lines of file(s).', add_help=False)
     parser.add_argument('--help', action='help', help='show this help message and exit')
     parser.add_argument('pattern', type=str, help='the pattern to find')
-    parser.add_argument('files', metavar='FILES', type=FileType('r'), nargs='*', default=[sys.stdin], help='the files(s) to search')
+    parser.add_argument('files', metavar='FILES', type=pathname, nargs='*', default=[sys.stdin], help='the files(s) to search')
     parser.add_argument('--color', '--colour', action='store_true', help='highlight pattern in output')
     parser.add_argument('-i', '--ignore-case', action='store_true', help='ignore case in search')
     parser.add_argument('-n', '--line-number', action='store_true', help='print line numbers, indexed beginning at 1')
     parser.add_argument('-h', '--no-filename', action='store_true', help='print output without filename headers')
     return parser
 
+
 if __name__ == '__main__':
     # set up argparse argument parser and get args
     parser = setup_parser()
     args = parser.parse_args()
     pattern = args.pattern
-    texts = args.files
+    paths = args.files
 
     # adjust no_filename
     if not args.no_filename:
-        if len(texts) == 1:
+        if len(paths) == 1:
             args.no_filename = True
 
     # assign find fn
@@ -71,24 +98,8 @@ if __name__ == '__main__':
         find = ignore_find
 
     # loop over files
-    for text in texts:
-        # setup for reading lines
-        line = text.readline()
-        lineno = 0 # counter for line number option
-        # read lines of a text
-        while line:
-            if args.line_number: # ugly lineno chunk
-                lineno += 1
-
-            if args.color:
-                result = color_find(pattern, line, find)
-            else:
-                index = find(pattern, line)
-                result = line if index != -1 else ''
-
-            # print to stdout and read next line
-            if len(result) > 0:
-                print_result(result, args.no_filename, text.name, lineno)
-            line = text.readline()
-
-        text.close()
+    for path in paths:
+        if 'mygrep:' not in path:
+            search_lines(open(path, 'r'), find, args.line_number, args.color, args.no_filename)
+        else:
+            sys.stdout.write(path)
