a01ab7576bf25c44391d9559ad1223aff1bbb1c8
Huyen Tran <hey.huyen@gmail.com>
2012-12-13 17:31:08 -0500

    Add -l, --files-with-matches option
    - This option only prints filenames
    - Fix handling of standard input arguments
    - Mixing conflicting flags not checked

--- mygrep.py
+++ mygrep.py
@@ -36,13 +36,13 @@ def color_find(pattern, string, find):
 def print_result(result, no_header, header, lineno):
     """Print found result to standard output."""
     if not no_header:
-        sys.stdout.write('%s:' % (header if header != '<stdin>' else '(standard input)'))
+        sys.stdout.write('%s:' % header)
     if lineno:
         sys.stdout.write('%d:' % lineno)
     sys.stdout.write(result)
 
-def grep_file(filename, pattern, find, line_number, color, no_filename):
-    text = sys.stdin if filename == '-' else open(filename, 'r')
+def grep_file(filename, pattern, find, only_names, line_number, color, no_filename):
+    text = sys.stdin if filename == '(standard input)' else open(filename, 'r')
     line = text.readline()
     lineno = 0 # counter for lineno option
     while line:
@@ -57,19 +57,23 @@ def grep_file(filename, pattern, find, line_number, color, no_filename):
 
         # print to stdout and read next line
         if len(result) > 0:
-            print_result(result, no_filename, text.name, lineno)
+            if only_names:
+                sys.stdout.write('%s\n' % filename)
+                break
+            else:
+                print_result(result, no_filename, filename, lineno)
         line = text.readline()
 
     text.close()
 
-def grep_files(paths, pattern, find, recurse, lineno, color, no_filename):
+def grep_files(paths, pattern, find, recurse, only_names, lineno, color, no_filename):
     for path in paths:
-        if os.path.isfile(path):
-            grep_file(path, pattern, find, lineno, color, no_filename)
+        if os.path.isfile(path) or path == '(standard input)':
+            grep_file(path, pattern, find, only_names, lineno, color, no_filename)
         else:
             if recurse:
                 more_paths = [path + '/' + child for child in os.listdir(path)]
-                grep_files(more_paths, pattern, find, recurse, lineno, color, no_filename)
+                grep_files(more_paths, pattern, find, recurse, only_names, lineno, color, no_filename)
             else:
                 sys.stdout.write('grep: %s: Is a directory\n' % path)
 
@@ -81,6 +85,7 @@ def setup_parser():
     parser.add_argument('--color', '--colour', action='store_true', help='highlight pattern in output')
     parser.add_argument('-h', '--no-filename', action='store_true', help='print output without filename headers')
     parser.add_argument('-i', '--ignore-case', action='store_true', help='ignore case in search')
+    parser.add_argument('-l', '--files-with-matches', action='store_true', help='only output the names of files containing the pattern.')
     parser.add_argument('-n', '--line-number', action='store_true', help='print line numbers, indexed beginning at 1')
     parser.add_argument('-R', '-r', '--recursive', action='store_true', help='recursively search directories')
     return parser
@@ -104,7 +109,8 @@ def main():
         find = ignore_find
 
     # what to do with files and dirs
-    grep_files(args.files, pattern, find, args.recursive, args.line_number, args.color, args.no_filename)
+    files = [f if f!= '-' else '(standard input)' for f in args.files]
+    grep_files(files, pattern, find, args.recursive, args.files_with_matches, args.line_number, args.color, args.no_filename)
 
 if __name__ == '__main__':
     main()
