e0aea02a980d706ef21968345435a5aa9e344ba0
Huyen Tran <hey.huyen@gmail.com>
2012-12-13 16:02:01 -0500

    Add recursive directory search
    - Remove pathname callback
    - Get filename args from arg parser and handle files/dirs/opening
    - Break out code more chunks into functions
    - mygrep results of recursive search not verified to match grep results.

--- mygrep.py
+++ mygrep.py
@@ -41,10 +41,10 @@ def print_result(result, no_header, header, lineno):
         sys.stdout.write('%d:' % lineno)
     sys.stdout.write(result)
 
-def search_lines(text, find, line_number, color, no_filename):
+def grep_file(filename, pattern, find, line_number, color, no_filename):
+    text = sys.stdin if filename == '-' else open(filename, 'r')
     line = text.readline()
-    lineno = 0 # counter for line number option
-    # read lines of a text
+    lineno = 0 # counter for lineno option
     while line:
         if line_number: # ugly lineno chunk
             lineno += 1
@@ -62,34 +62,40 @@ def search_lines(text, find, line_number, color, no_filename):
 
     text.close()
 
-def pathname(path):
-    if os.path.isfile(path):
-        return path
-    else:
-        return 'mygrep: %s: Is a directory\n' % path
+def grep_files(paths, pattern, find, recurse, lineno, color, no_filename):
+    for path in paths:
+        if os.path.isfile(path):
+            grep_file(path, pattern, find, lineno, color, no_filename)
+        else:
+            if recurse:
+                more_paths = [path + '/' + child for child in os.listdir(path)]
+                grep_files(more_paths, pattern, find, recurse, lineno, color, no_filename)
+            else:
+                sys.stdout.write('grep: %s: Is a directory\n' % path)
 
 def setup_parser():
     parser = ArgumentParser(description='Find occurrences of a pattern in lines of file(s).', add_help=False)
     parser.add_argument('--help', action='help', help='show this help message and exit')
     parser.add_argument('pattern', type=str, help='the pattern to find')
-    parser.add_argument('files', metavar='FILES', type=pathname, nargs='*', default=[sys.stdin], help='the files(s) to search')
+    parser.add_argument('files', metavar='FILES', nargs='*', default=['-'], help='the files(s) to search')
     parser.add_argument('--color', '--colour', action='store_true', help='highlight pattern in output')
+    parser.add_argument('-h', '--no-filename', action='store_true', help='print output without filename headers')
     parser.add_argument('-i', '--ignore-case', action='store_true', help='ignore case in search')
     parser.add_argument('-n', '--line-number', action='store_true', help='print line numbers, indexed beginning at 1')
-    parser.add_argument('-h', '--no-filename', action='store_true', help='print output without filename headers')
+    parser.add_argument('-R', '-r', '--recursive', action='store_true', help='recursively search directories')
     return parser
 
-
-if __name__ == '__main__':
+def main():
     # set up argparse argument parser and get args
     parser = setup_parser()
     args = parser.parse_args()
     pattern = args.pattern
-    paths = args.files
 
     # adjust no_filename
     if not args.no_filename:
-        if len(paths) == 1:
+        if args.recursive:
+            args.no_filename = False
+        elif len(args.files) == 1:
             args.no_filename = True
 
     # assign find fn
@@ -97,9 +103,8 @@ if __name__ == '__main__':
     if args.ignore_case:
         find = ignore_find
 
-    # loop over files
-    for path in paths:
-        if 'mygrep:' not in path:
-            search_lines(open(path, 'r'), find, args.line_number, args.color, args.no_filename)
-        else:
-            sys.stdout.write(path)
+    # what to do with files and dirs
+    grep_files(args.files, pattern, find, args.recursive, args.line_number, args.color, args.no_filename)
+
+if __name__ == '__main__':
+    main()
