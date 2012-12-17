4f7e081a842854102abd9ac8a84f3a6c11c84998
Huyen Tran <hey.huyen@gmail.com>
2012-12-17 10:57:35 -0500

    Add boyer_moore.py file, slim version of mygrep.py
    - Base for implementing boyer-moore string search algorithm

---
+++ boyer_moore.py
@@ -0,0 +1,82 @@
+import sys, os
+from argparse import ArgumentParser, FileType
+
+GREEN = '\033[32m'
+PRETTY_GREEN = '\033[92m'
+END = '\033[0m'
+
+def index_find(pattern, string):
+    """Find first occurence of pattern in string."""
+    for i in range(len(string)):
+        for j in range(len(pattern)):
+            if string[i+j] != pattern[j]:
+                break
+            elif j == len(pattern) - 1:
+                return i
+    return -1
+
+def color_find(pattern, string, find):
+    """Find and color all occurences of pattern in string."""
+    result = ''
+    index = find(pattern, string)
+
+    while index != -1:
+        result += string[:index]
+        result += PRETTY_GREEN + string[index:index + len(pattern)] + END
+        string = string[index + len(pattern):]
+        index = find(pattern, string)
+
+    return result if result == '' else result + string
+
+def grep_file(filename, pattern, find, color):
+    text = sys.stdin if filename == '(standard input)' else open(filename, 'r')
+    line = text.readline()
+    while line:
+        if color:
+            result = color_find(pattern, line, find)
+        else:
+            index = find(pattern, line)
+            result = line if index != -1 else ''
+
+        # print to stdout and read next line
+        if len(result) > 0:
+            sys.stdout.write('%s:%s' % (filename, result))
+        line = text.readline()
+
+    text.close()
+
+def grep_files(paths, pattern, find, recurse, color):
+    for path in paths:
+        if os.path.isfile(path) or path == '(standard input)':
+            grep_file(path, pattern, find, color)
+        else:
+            if recurse:
+                more_paths = [path + '/' + child for child in os.listdir(path)]
+                grep_files(more_paths, pattern, find, recurse, color)
+            else:
+                sys.stdout.write('grep: %s: Is a directory\n' % path)
+
+def setup_parser():
+    parser = ArgumentParser(description='Find occurrences of a pattern in lines of file(s).', add_help=False)
+    parser.add_argument('--help', action='help', help='show this help message and exit')
+    parser.add_argument('pattern', type=str, help='the pattern to find')
+    parser.add_argument('files', metavar='FILES', nargs='*', default=['-'], help='the files(s) to search')
+    parser.add_argument('--color', '--colour', action='store_true', help='highlight pattern in output')
+    parser.add_argument('-R', '-r', '--recursive', action='store_true', help='recursively search directories')
+    return parser
+
+def main():
+    # set up argparse argument parser and get args
+    parser = setup_parser()
+    args = parser.parse_args()
+    pattern = args.pattern
+
+    # assign find fn
+    find = index_find
+
+    # what to do with files and dirs
+    files = [f if f!= '-' else '(standard input)' for f in args.files]
+    grep_files(files, pattern, find, args.recursive, args.color)
+
+if __name__ == '__main__':
+    main()
