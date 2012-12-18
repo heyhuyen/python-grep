eeb2fdcd24d5d9f5915de556cde5cc24b52c2e10
Huyen Tran <hey.huyen@gmail.com>
2012-12-18 18:25:48 -0500

    Refactor
    - rename variables, functions
    - add functions to route find options
    - restrict printing to print function
    - handle option conflict for printing
    - reformat lines lengths to <= 79 chars

--- mygrep.py
+++ mygrep.py
@@ -1,13 +1,12 @@
 import sys, os
 from argparse import ArgumentParser, FileType
-import pdb
 
 GREEN = '\033[32m'
 PRETTY_GREEN = '\033[92m'
-END = '\033[0m'
+END_COLOR = '\033[0m'
 
 def index_find(pattern, string):
-    """Find first occurence of pattern in string."""
+    """Find index of pattern match in string. Returns -1 if not found."""
     for i in range(len(string)):
         for j in range(len(pattern)):
             if string[i+j] != pattern[j]:
@@ -16,101 +15,133 @@ def index_find(pattern, string):
                 return i
     return -1
 
-def ignore_find(pattern, string):
-    """Ignore case."""
+def ignore_case_find(pattern, string):
+    """Look for pattern in string, ignoring case."""
     return index_find(pattern.lower(), string.lower())
 
-def color_find(pattern, string, find):
-    """Find and color all occurences of pattern in string."""
+def find(pattern, string, ignore_case):
+    """Find the pattern in the string. Return -1 if not found."""
+    if ignore_case:
+        return ignore_case_find(pattern, string)
+    else:
+        return index_find(pattern, string)
+
+def color_find(pattern, string, ignore_case):
+    """Find all matches of pattern in string. Returns colored string, or empty string if not found."""
     result = ''
-    index = find(pattern, string)
+    index = find(pattern, string, ignore_case)
 
     while index != -1:
         result += string[:index]
-        result += PRETTY_GREEN + string[index:index + len(pattern)] + END
+#------------------------------------------------------------------------------
+        result += PRETTY_GREEN + string[index:index + len(pattern)] + END_COLOR
         string = string[index + len(pattern):]
-        index = find(pattern, string)
+        index = find(pattern, string, ignore_case)
 
     return result if result == '' else result + string
 
-def print_result(result, no_header, header, lineno):
-    """Print found result to standard output."""
-    if not no_header:
-        sys.stdout.write('%s:' % header)
-    if lineno:
-        sys.stdout.write('%d:' % lineno)
-    sys.stdout.write(result)
+def get_match(pattern, string, color, ignore_case):
+    """Find the pattern in the string. Returns the match result or the empty string if not found."""
+    if color:
+        return color_find(pattern, string, ignore_case)
+    else:
+        index = find(pattern, string, ignore_case)
+        return string if index != -1 else ''
 
-def grep_file(filename, pattern, find, only_names, line_number, color, no_filename):
-    text = sys.stdin if filename == '(standard input)' else open(filename, 'r')
-    line = text.readline()
-    lineno = 0 # counter for lineno option
-    while line:
-        if line_number: # ugly lineno chunk
-            lineno += 1
-
-        if color:
-            result = color_find(pattern, line, find)
-        else:
-            index = find(pattern, line)
-            result = line if index != -1 else ''
-
-        # print to stdout and read next line
+def print_result(print_header, header, print_lineno, lineno, print_line, line):
+    """Print result to standard output."""
+    result = ''
+    if print_header:
+        result += '%s' % header
+    if print_lineno:
         if len(result) > 0:
-            if only_names:
-                sys.stdout.write('%s\n' % filename)
+            result += ':'
+        result += '%d' % lineno
+    if print_line:
+        if len(result) > 0:
+            result += ':'
+        result += line
+    sys.stdout.write('%s\n' % result.strip('\n'))
+
+def grep_file(filename, pattern, color, ignore_case, print_headers,
+                print_lineno, print_lines):
+    """Search a single file or standard input."""
+    text = sys.stdin if filename == '(standard input)' else open(filename, 'r')
+
+    line = text.readline()
+    lineno = 1
+    while line:
+        result = get_match(pattern, line, color, ignore_case)
+        if len(result) > 0:
+            print_result(print_headers, filename, print_lineno, lineno,
+                            print_lines, result)
+            if print_headers and not print_lines: # files-with-matches option
                 break
-            else:
-                print_result(result, no_filename, filename, lineno)
         line = text.readline()
+        lineno += 1
 
     text.close()
 
-def grep_files(paths, pattern, find, recurse, only_names, lineno, color, no_filename):
+def grep_files(paths, pattern, recurse, color, ignore_case, print_headers,
+                print_lineno, print_line):
+    """Search files and directories."""
     for path in paths:
         if os.path.isfile(path) or path == '(standard input)':
-            grep_file(path, pattern, find, only_names, lineno, color, no_filename)
+            grep_file(path, pattern, color, ignore_case, print_headers,
+                        print_lineno, print_line)
         else:
             if recurse:
                 more_paths = [path + '/' + child for child in os.listdir(path)]
-                grep_files(more_paths, pattern, find, recurse, only_names, lineno, color, no_filename)
+                grep_files(more_paths, pattern, recurse, color, ignore_case,
+                            print_headers, print_lineno, print_line)
             else:
                 sys.stdout.write('grep: %s: Is a directory\n' % path)
 
 def setup_parser():
-    parser = ArgumentParser(description='Find occurrences of a pattern in lines of file(s).', add_help=False)
-    parser.add_argument('--help', action='help', help='show this help message and exit')
+    """Configure command line argument parser object."""
+    parser = ArgumentParser(description='Find matches of a pattern in ' \
+                            'lines of file(s).', add_help=False)
+    parser.add_argument('--help', action='help', help='show this help ' \
+                        'message and exit')
     parser.add_argument('pattern', type=str, help='the pattern to find')
-    parser.add_argument('files', metavar='FILES', nargs='*', default=['-'], help='the files(s) to search')
-    parser.add_argument('--color', '--colour', action='store_true', help='highlight pattern in output')
-    parser.add_argument('-h', '--no-filename', action='store_true', help='print output without filename headers')
-    parser.add_argument('-i', '--ignore-case', action='store_true', help='ignore case in search')
-    parser.add_argument('-l', '--files-with-matches', action='store_true', help='only output the names of files containing the pattern.')
-    parser.add_argument('-n', '--line-number', action='store_true', help='print line numbers, indexed beginning at 1')
-    parser.add_argument('-R', '-r', '--recursive', action='store_true', help='recursively search directories')
+    parser.add_argument('files', metavar='FILES', nargs='*', default=['-'],
+                        help='the files(s) to search')
+    parser.add_argument('--color', '--colour', action='store_true',
+                        help='highlight matches')
+    parser.add_argument('-h', '--no-filename', action='store_true',
+                        help='print without filename headers')
+    parser.add_argument('-i', '--ignore-case', action='store_true',
+                        help='case-insensitive search')
+    parser.add_argument('-l', '--files-with-matches', action='store_true',
+                        help='print only filenames with matches')
+    parser.add_argument('-n', '--line-number', action='store_true',
+                        help='print line numbers, indexed from 1')
+    parser.add_argument('-R', '-r', '--recursive', action='store_true',
+                        help='recursively search directories')
     return parser
 
+DEFAULT_PRINT_OPTIONS = (False, False, True)
+
 def main():
-    # set up argparse argument parser and get args
     parser = setup_parser()
     args = parser.parse_args()
     pattern = args.pattern
-
-    # adjust no_filename
-    if not args.no_filename:
-        if args.recursive:
-            args.no_filename = False
-        elif len(args.files) == 1:
-            args.no_filename = True
-
-    # assign find fn
-    find = index_find
-    if args.ignore_case:
-        find = ignore_find
-
-    # what to do with files and dirs
     files = [f if f!= '-' else '(standard input)' for f in args.files]
-    grep_files(files, pattern, find, args.recursive, args.files_with_matches, args.line_number, args.color, args.no_filename)
+
+    print_headers, print_lineno, print_lines = DEFAULT_PRINT_OPTIONS
+    if args.files_with_matches:
+        print_headers = True
+        print_lines = False
+    else:
+        if args.recursive or len(files) > 1:
+            print_headers = True
+        if args.line_number:
+            print_lineno = True
+        if args.no_filename:
+            print_headers = False
+
+    grep_files(files, pattern, args.recursive, args.color, args.ignore_case,
+                print_headers, print_lineno, print_lines)
 
 if __name__ == '__main__':
     main()
